7ff63c4be9ce2d494edfbb170a5678864711077d
André <andrecorsopossan@gmail.com>
2023-08-04 18:04:24 -0300

    Header close in click function

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -2,3 +2,11 @@ const header__hamburger = document.querySelector(".header__hamburger");
 const nav = document.querySelector(".header__nav");
 
 header__hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+const headerNavList = document.querySelector(".header__nav-list");
+
+headerNavList.addEventListener("click", (event) => {
+    const eventTarget = event.target;
+
+    eventTarget.tagName == "A" ? nav.classList.toggle("active") : false;
+});
